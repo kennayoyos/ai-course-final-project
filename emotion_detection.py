08ec544cc19e79c7e7f473f1a92e92dcc3fba94c
Kenneth Nayoyos <ken.nayoyos@outlook.com>
2026-07-12 11:20:12 -0400

    Complete task 3 for the assessment

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,5 +10,24 @@ def emotion_detector(text_to_analyze):
 
     # API call to the Watson NLP Library
     response = requests.post(url, json = input_json, headers = headers, timeout = 5)
+    json_response = json.loads(response.text)
+    emotions = json_response['emotionPredictions'][0]['emotion']
 
-    return response.text
+    # Find the strongest emotion
+    dominant_emotion = ('', 0)
+    for emotion, score in emotions.items():
+        _, max_score = dominant_emotion
+        if score > max_score:
+            dominant_emotion = (emotion, score)
+
+    # Prepare final response object
+    final_response = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion[0]
+    }
+
+    return final_response
